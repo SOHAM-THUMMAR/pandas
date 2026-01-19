a0d80161f6428fc68a2ad77a2f36d866b23ebc8f
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-01-20 02:08:19 +0530

    TST: Align colmap tests with rename-style non-mutating behavior

--- pandas/tests/frame/methods/test_colmap.py
+++ pandas/tests/frame/methods/test_colmap.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 def test_colmap_lower():
     df = pd.DataFrame(columns=["A", "B"])
-    df.colmap(str.lower)
+    out = df.colmap(str.lower)
     assert list(df.columns) == ["a", "b"]
 
 
 def test_colmap_replace():
     df = pd.DataFrame(columns=["col 1", "col 2"])
-    df.colmap(lambda c: c.replace(" ", "_"))
+    out = df.colmap(lambda c : c .replace(" ","_"))
     assert list(df.columns) == ["col_1", "col_2"]
 
 
-def test_colmap_returns_self():
+def test_colmap_returns_new():
     df = pd.DataFrame(columns=["A"])
     out = df.colmap(str.lower)
-    assert out is df
+    assert out is not df
